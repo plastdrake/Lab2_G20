66aacdad1f58222aaa33671e32932eaa08d9185b
Bundy88 <agrytfors@hotmail.com>
2024-10-17 10:25:43 +0200

    Added a couple of controllers

---
+++ Lab2_G20/Controllers/PlantingScheduleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_G20.Controllers
+{
+    public class PlantingScheduleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
