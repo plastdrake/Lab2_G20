226a32808b9c231b2e8a8cd47d833950e8b4ee12
Bundy88 <agrytfors@hotmail.com>
2024-10-21 11:06:08 +0200

    Added a couple of controllers

---
+++ Lab2_G20/Controllers/NotificationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_G20.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
