eade2be091692b1bdc14ba13ba8bb5d8eaf52edb
Sebastian Svensson <sebastian@rosmic.se>
2024-10-22 09:48:41 +0200

    Adding possibility to manually enter harvest date

--- Lab2_G20/Models/Crop.cs
+++ Lab2_G20/Models/Crop.cs
@@ -10,9 +10,33 @@ namespace Lab2_G20.Models
         public DateTime HarvestDate { get; set; }
 
         // Beräknad egenskap för att få skördedatum baserat på planteringsdatum och dagar till skörd
-        public void SetHarvestDate(int daysToHarvest)
+        public bool SetHarvestDate(int daysToHarvest)
         {
-            HarvestDate = PlantingDate.AddDays(daysToHarvest);
+            // Kontrollera om daysToHarvest är ett giltigt värde
+            if (daysToHarvest <= 0)
+            {
+                throw new ArgumentException("Dagar till skörd måste vara större än 0.");
+            }
+
+            // Kontrollera om PlantingDate är ett giltigt datum
+            if (PlantingDate == DateTime.MinValue)
+            {
+                throw new InvalidOperationException("Planteringsdatumet är ogiltigt.");
+            }
+
+            // Beräkna skördedatum
+            DateTime calculatedHarvestDate = PlantingDate.AddDays(daysToHarvest);
+
+            // Kontrollera om skördedatumet är i framtiden
+            if (calculatedHarvestDate <= DateTime.Now)
+            {
+                throw new InvalidOperationException("Skördedatumet måste vara i framtiden.");
+            }
+
+            // Om alla kontroller passerar, sätt skördedatumet
+            HarvestDate = calculatedHarvestDate;
+
+            return true; // Indikerar att skördedatumet har satts korrekt
         }
     }
 }
