d1dc81fa29166ae6c8e61f6583c2ecf3f0e6e95c
Bundy88 <agrytfors@hotmail.com>
2024-10-23 10:09:58 +0200

    Restoring files

--- Lab2_G20/Models/PlantingSchedule.cs
+++ Lab2_G20/Models/PlantingSchedule.cs
@@ -7,7 +7,7 @@
         public string PlantingDate { get; set; }  // Date of planting
         public string OptimalPlantingDate { get; set; }  // Calculated optimal planting date
         public int ReminderDaysBefore { get; set; }  // Days before planting to send reminders
-        public string DaysToHarvest { get; set; }  // Number of days till harvest
+        public int DaysToHarvest { get; set; }  // Number of days till harvest
         public string Notes { get; set; }  // Any additional notes about the crop
     }
 }
