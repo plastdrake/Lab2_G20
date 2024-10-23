bcd57dcee91d4c2bef759165a690a841875aaf56
Bundy88 <agrytfors@hotmail.com>
2024-10-23 09:26:33 +0200

    Add DaysToHarvest to PlantingSchedule

--- Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
+++ Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e77a44c5679a521252bca6747d93e5d6f001a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52bf5ca5dffd00174d18ce82514935c18fe1edf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
