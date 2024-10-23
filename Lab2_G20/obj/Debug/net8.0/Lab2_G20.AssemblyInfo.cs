558eb9e8cb43754362ea70d80dbaf3e9a6f4ff30
Sebastian Svensson <sebastian@rosmic.se>
2024-10-23 09:52:55 +0200

    Adding razor pages for planting schedule

--- Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
+++ Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd57dcee91d4c2bef759165a690a841875aaf56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d20fc3b533d376f7d8f09736836dc7291ef342")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
