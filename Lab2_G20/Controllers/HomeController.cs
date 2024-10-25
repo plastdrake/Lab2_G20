73e03eb16f3d6126aeb15afe2e900066ff8cd9fb
Bundy88 <agrytfors@hotmail.com>
2024-10-25 10:18:29 +0200

    Added reminder button on home page

--- Lab2_G20/Controllers/HomeController.cs
+++ Lab2_G20/Controllers/HomeController.cs
@@ -10,17 +10,23 @@ namespace Lab2_G20.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly WeatherService _weatherService;
+        private readonly NotificationsController _notificationsController;
 
-        public HomeController(ILogger<HomeController> logger, WeatherService weatherService)
+        public HomeController(ILogger<HomeController> logger, WeatherService weatherService, NotificationsController notificationsController)
         {
             _logger = logger;
             _weatherService = weatherService;
+            _notificationsController = notificationsController;
         }
 
         public async Task<IActionResult> Index()
         {
             var weatherData = await _weatherService.GetWeatherDataAsync(); // Fetch weather data
             ViewBag.Weather = weatherData; // Store it in ViewBag
+
+            // Check if a reminder is due and set it in ViewBag
+            ViewBag.IsReminderDue = _notificationsController.CheckIfReminderDue();
+
             return View();
         }
 
