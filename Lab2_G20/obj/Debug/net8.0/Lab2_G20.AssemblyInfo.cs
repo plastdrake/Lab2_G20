bfbabc16abfbcc4e0fa343ac0bfc27ef957ba0cb
Bundy88 <agrytfors@hotmail.com>
2024-10-29 12:42:15 +0100

    Tidying up the code (Growth history)

--- Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
+++ Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74418a31e32d0fa798941b1c87d2a3d80810f80c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31cac0cf7c91783584ea5c4f1b44a617a2d08e6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
