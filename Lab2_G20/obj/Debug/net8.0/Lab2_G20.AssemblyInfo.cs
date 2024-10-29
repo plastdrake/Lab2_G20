3f5995531fb2a210325da3c024457f94300d66d7
Bundy88 <agrytfors@hotmail.com>
2024-10-29 13:05:25 +0100

    Update growthHistory

--- Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
+++ Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cecfbfdbceb713d3ca1be0cb471ab7b1cf8d24da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44bf797cf0a1f0dd570dadde1581787ce3e6541b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
