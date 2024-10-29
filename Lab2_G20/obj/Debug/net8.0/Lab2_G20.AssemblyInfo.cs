44bf797cf0a1f0dd570dadde1581787ce3e6541b
Bundy88 <agrytfors@hotmail.com>
2024-10-29 12:54:08 +0100

    HarvestTracking changes to html file

--- Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
+++ Lab2_G20/obj/Debug/net8.0/Lab2_G20.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4269034353c0a2f665bedae4200a429d59b1652d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cecfbfdbceb713d3ca1be0cb471ab7b1cf8d24da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2_G20")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
